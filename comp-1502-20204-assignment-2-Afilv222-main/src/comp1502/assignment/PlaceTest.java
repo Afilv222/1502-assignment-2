4371aaf03375c46f2036f9dc4c54078899f0dd46
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2020-11-18 21:32:46 -0700

    Made some tests for stoping,checking if person on the elevator or not

--- comp-1502-20204-assignment-2-Afilv222-main/src/comp1502/assignment/PlaceTest.java
+++ comp-1502-20204-assignment-2-Afilv222-main/src/comp1502/assignment/PlaceTest.java
@@ -74,6 +74,7 @@ class PlaceTest {
 		
 		p.exit();
 		assertFalse(p.hasPerson());
+		assertNull(p.getPerson());
 		
 		assertEquals("Test-Nobody",p.toString());
 	}
@@ -155,4 +156,45 @@ class PlaceTest {
 		e.move();
 		assertEquals(floors.get(1), e.getCurrentFloor());
 	}
+	
+	@Test
+	void testMoveDownStops() {
+		Person personOnFloor = new Person("Afil");
+		Place  place = new Place(personOnFloor);
+		ArrayList<Floor> floors = new ArrayList<>();
+		floors.add(new Floor("1",personOnFloor));
+		floors.add(new Floor("2",null));
+		floors.add(new Floor("3",null));
+		
+		String name = "Test";
+		
+		Elevator e = new Elevator(name, floors,null);
+		
+		e.call(floors.get(2));
+		e.move();
+		e.move();
+		e.call(floors.get(1));
+		e.move();
+		e.move();
+		assertEquals(floors.get(1), e.getCurrentFloor());
+		assertFalse(e.isMoving());
+	}
+	
+	@Test 
+	void testToStringWithoutPersonOnFloor() {
+		String expectedName = "Test";
+		Floor f  = new Floor(expectedName,null);
+		assertEquals(expectedName+"-Nobody", f.toString());
+	}
+	
+	@Test 
+	void testToStringWithPersonOnFloor() {
+		Person personOnFloor = new Person("Afil");
+		String expectedName = "Test";
+		Floor f  = new Floor(expectedName,personOnFloor);
+	
+		f.arrive(personOnFloor);
+		assertEquals(f.getName()+"-"+personOnFloor.getName(), f.toString());
+	}
+	
 }
