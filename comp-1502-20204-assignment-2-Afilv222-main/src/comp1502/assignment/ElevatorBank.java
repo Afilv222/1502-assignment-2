621a58f6df516c4338a7019d479fa17c43d051cd
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2020-11-22 23:18:46 -0700

    Added call,move and toString method
    Started call method

--- comp-1502-20204-assignment-2-Afilv222-main/src/comp1502/assignment/ElevatorBank.java
+++ comp-1502-20204-assignment-2-Afilv222-main/src/comp1502/assignment/ElevatorBank.java
@@ -5,10 +5,34 @@ public class ElevatorBank {
 
 	
 	private List<Elevator> elevators = new ArrayList<Elevator>();
-	
+	private Elevator elevator; 
+	 
 	
 	public ElevatorBank(ArrayList<Elevator> elevators) {
 		this.elevators = elevators;
 	}
 	
+	
+	
+	public void call(Floor floor) {
+		int currentSmallestIndex = 0; 
+		
+		for(int i = 0; i < elevators.size(); i++) {
+			
+			if(elevators.get(i).distance(floor) < elevators.get(currentSmallestIndex ).distance(floor)) {
+				currentSmallestIndex = i;
+			}
+	
+		}
+		 elevators.get(currentSmallestIndex).call(floor);
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public String toString() {
+		return "";
+	}
 }
