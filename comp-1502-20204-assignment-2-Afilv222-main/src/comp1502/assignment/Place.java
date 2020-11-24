229fc473a1073d7863e35cdf296330e3c648c848
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2020-11-24 15:03:57 -0700

    Added some java doc

--- comp-1502-20204-assignment-2-Afilv222-main/src/comp1502/assignment/Place.java
+++ comp-1502-20204-assignment-2-Afilv222-main/src/comp1502/assignment/Place.java
@@ -1,6 +1,8 @@
 package comp1502.assignment;
 /**
- * This 
+ * Place class , is super class of Elevator and Floor which provides 
+ * a concept of a location where a person can wait. But can also tell Person 
+ * to enter or exit Elevator same thing goes for Floor.
  * @author afil_Vidyasagar
  *
  */
@@ -8,19 +10,27 @@ public class Place {
 	private Person person; 
 	
 	
-	
+	/**
+	 * Created constructor which initialize the instance variables 
+	 * @param person equal to the instance variable person
+	 * 
+	 */
 	public Place(Person person) {
 		this.person = person; 
 	}
+	
+	
 	/**
-	 * @return the waiter
+	 * 
+	 * Gets the person.  
+	 * @return the person
 	 */
 	public Person getPerson() { 
 		return person;
 	}
 	
 	/**
-	 * 
+	 * Checks if the elevator or floor has person currently riding the elevator or waiting on the floor.
 	 * @return
 	 */
 	public boolean hasPerson() {
@@ -28,6 +38,11 @@ public class Place {
 		
 	}
 	
+	/**
+	 * The person on the floor should now enter the elevator.
+	 * @param person
+	 * @return person equal to personInElevator.
+	 */
 	public void arrive(Person newPerson) { 
 		person = newPerson;
 	}
