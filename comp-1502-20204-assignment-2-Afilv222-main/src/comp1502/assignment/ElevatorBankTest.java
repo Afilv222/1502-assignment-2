cabcde550b5d5817e5fbad97501036d17e7f8177
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2020-11-24 01:49:54 -0700

    Updated some changes

--- comp-1502-20204-assignment-2-Afilv222-main/src/comp1502/assignment/ElevatorBankTest.java
+++ comp-1502-20204-assignment-2-Afilv222-main/src/comp1502/assignment/ElevatorBankTest.java
@@ -34,28 +34,40 @@ class ElevatorBankTest {
 		ArrayList<Floor> floors = new ArrayList<>();
 		
 		floors.add(new Floor("1",personOnFloor) ); 
-		floors.add(new Floor("2",null));  // Ele1 should be here
+		floors.add(new Floor("2",null));  
 		floors.add(new Floor("3",null));
-		floors.add(new Floor("4",null)); /// Ele2 should be here
+		floors.add(new Floor("4",null)); 
 		
-		//Elevator elevator1 = new Elevator("Ele1",floors,null,floors.get(1));
-		//Elevator elevator2 = new Elevator("Ele2",floors,null,floors.get(3));
 		ArrayList<Elevator> elevators = new ArrayList<Elevator>();
 		elevators.add(new Elevator("Ele1",floors,null,floors.get(1)));
-		//elevators.add(elevator1);
 		elevators.add(new Elevator("Ele2",floors,null,floors.get(3)));
 		
 		ElevatorBank EB = new ElevatorBank(elevators);
 		
 		EB.call(floors.get(0));
-		//EB.move();
+		EB.move();
 		
-		//assertFalse(elevators.get(0).isMoving());
+		assertFalse(elevators.get(0).isMoving());
 		assertEquals(floors.get(0),elevators.get(0).getDestinationFloor());
 		assertEquals(floors.get(0),elevators.get(0).getCurrentFloor());
 		
 		assertFalse(elevators.get(1).isMoving());
 		assertNull(elevators.get(1).getDestinationFloor());
 		assertEquals(floors.get(3),elevators.get(1).getCurrentFloor());
+	}
+	
+	@Test
+	void elevatorsAtTheSameDistance() {
+		
+		Person personOnFloor = new Person("Shreshth"); 
+		Place  place = new Place(personOnFloor);
+		ArrayList<Floor> floors = new ArrayList<>();
+		
+		floors.add(new Floor("1",personOnFloor) ); 
+		floors.add(new Floor("2",null));  
+		floors.add(new Floor("3",null));
+		floors.add(new Floor("4",null)); // Ele1 should be here /// Ele2 should be here 
+		floors.add(new Floor("5",null)); 
+		floors.add(new Floor("6",null)); 
 	}
 }
