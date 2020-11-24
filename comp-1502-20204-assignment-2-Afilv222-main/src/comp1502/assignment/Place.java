35424349ead6eed06b00ad1c7182580ffd8bdd63
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2020-11-24 15:18:07 -0700

    Added some java doc

--- comp-1502-20204-assignment-2-Afilv222-main/src/comp1502/assignment/Place.java
+++ comp-1502-20204-assignment-2-Afilv222-main/src/comp1502/assignment/Place.java
@@ -39,21 +39,29 @@ public class Place {
 	}
 	
 	/**
-	 * The person on the floor should now enter the elevator.
-	 * @param person
-	 * @return person equal to personInElevator.
+	 * Sets a person waiting on a floor or
+	 * The person on the floor should now enter the elevator. 
+	 * @param newPerson equals the person instance variable 
 	 */
 	public void arrive(Person newPerson) { 
 		person = newPerson;
 	}
 	
+	/**
+	 *  When the person leaves the elevator or floor the elevator or floor will no longer have someone on board.
+	 *  Updates the person in the elevator or floor to null , meaning the person in the elevator or floor is no longer there.
+	 *  Also the person that exited the elevator or floor should be the same person that entered. 
+	 * @return personInElevator on the floor.
+	 */
 	public Person exit() { 
 		Person departer = person;
 		person = null;
 		return departer;
 	}
 	
-	
+	/**
+	 * The super class(Place) will return the object into String which will display ther
+	 */
 	public String toString() {
 		if (this.person == null) {
 			return "-Nobody";
