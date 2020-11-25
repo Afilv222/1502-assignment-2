9ee4dd66f9f080a90078aece4e70caa5001f709e
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2020-11-24 20:14:19 -0700

    Fixed some changes in simlation class

--- comp-1502-20204-assignment-2-Afilv222-main/src/comp1502/assignment/Simulation.java
+++ comp-1502-20204-assignment-2-Afilv222-main/src/comp1502/assignment/Simulation.java
@@ -13,9 +13,15 @@ public class Simulation {
 		floors.add(new Floor("C",null));
 		floors.add(new Floor("D",null));
 		
-		Elevator e = new Elevator("Simulation Elevator", floors,null);
+		///Elevator e = new Elevator("Simulation Elevator", floors,null);
+		
+		ArrayList<Elevator> elevators = new ArrayList<Elevator>();
+		elevators.add(new Elevator("Ele1",floors,null,floors.get(1)));
+		elevators.add(new Elevator("Ele2",floors,null,floors.get(3)));
+		ElevatorBank EB = new ElevatorBank(elevators);
+		
 		//Person p = new Person("Persephony");
-		Building b = new Building("Simulation Building", floors, e, personOnFloor );
+		Building b = new Building("Simulation Building", floors, EB, personOnFloor );
 		
 		for (int i = 1; i <= 100; i++) {
 			System.out.println("Step " + i);
